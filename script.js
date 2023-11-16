00669b4e951e4fd13f8cbd74e9979af41ba76609
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-11-16 11:28:05 +0000

    setting up javascript

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ const questions = [
     },
     // Add more questions as needed
   ];
+  
+  let currentQuestionIndex = 0;
+  
+  function loadQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    document.getElementById("question").innerText = currentQuestion.question;
+  }
+  
+  function checkAnswer() {
+    const userAnswer = document.getElementById("answerInput").value.trim().toLowerCase();
+    const currentQuestion = questions[currentQuestionIndex];
+  
+    if (userAnswer === currentQuestion.correctAnswer.toLowerCase()) {
+      alert("Correct!");
+    } else {
+      alert(`Incorrect. The correct answer is: ${currentQuestion.correctAnswer}`);
+    }
+  
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      loadQuestion();
+    } else {
+      alert("Quiz completed!");
+      resetQuiz();
+    }
+  }
+  
+  function nextQuestion() {
+    checkAnswer();
+  }
+  
+  function resetQuiz() {
+    currentQuestionIndex = 0;
+    loadQuestion();
+  }
+  
