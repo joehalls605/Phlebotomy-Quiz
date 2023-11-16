ae1ccd30fb54e54cad4e78b51994f91604b8c546
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-11-16 11:20:31 +0000

    Base app set-up

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+      question: "What does CBC stand for in phlebotomy?",
+      correctAnswer: "Complete Blood Count"
+    },
+    {
+      question: "Which vein is commonly used for venipuncture?",
+      correctAnswer: "Median cubital vein"
+    },
+    // Add more questions as needed
+  ];
