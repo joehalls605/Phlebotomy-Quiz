8431c3278366b430530f94f7b7cb4c6b95002381
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-11-24 17:07:41 +0000

    Removed duplicate shuffleQuestions()

--- script.js
+++ script.js
@@ -162,12 +162,7 @@ function hideHint() {
   hintElement.textContent = "";
 }
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+
 
 function checkAnswer() {
   const userAnswer = document.getElementById("answerInput").value.trim();
